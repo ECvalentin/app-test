384346e01b95126195d20dd81ea9323f3adf180c
ni-trix <valent.23@hotmail.fr>
2022-02-02 00:53:59 +0100

    start responsive navbar / add font awesome

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  navDisplay: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toogleNav(){
+    this.navDisplay = !this.navDisplay
   }
 
 }
